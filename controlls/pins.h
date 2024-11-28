51498d926f1c904ab93af710ed300e1caca07e7b
Mylonas Dimitris <milonasdimitris9@gmail.com>
2024-11-28 15:34:59 +0200

    Robot's controlls

--- controlls/pins.h
+++ controlls/pins.h
@@ -2,12 +2,12 @@
 #define PINS_H
 
 // Motors' PWM pins
-#define motorL 3
+#define motorL 9
 #define motorB 6
 #define motorR 5
 
 // Motors' Direction pins
-#define enableL 2
+#define enableL 8
 #define enableB 7
 #define enableR 4
 
